2d5046e0eb6b1abe4e30b2cff1ce2500bc52ef52
SinaKoulani <kolanisina727@gmail.com>
2026-09-20 15:08:10 +0330

    Add GitHub Pages deploy workflow

--- src/App.tsx
+++ src/App.tsx
@@ -12,8 +12,8 @@ const App = () => {
   return (
     <Routes>
       <Route element={<Layout />}>
-        <Route path="/about" element={<About />} />
         <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
         <Route path="/projects" element={<Projects />} />
         <Route path="/projects/:id" element={<ProjectDetail />} />
         <Route path="/experience" element={<Experience />} />
